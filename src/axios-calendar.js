72e007be623904fb53ce69b7ba7f30a251ad0cd3
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2022-03-08 21:33:43 +0600

    изменен baseUrl

--- src/axios-calendar.js
+++ src/axios-calendar.js
@@ -1,15 +1,16 @@
-import axios from 'axios';
-import store from './store/configStore';
+import axios from "axios";
+import store from "./store/configStore";
 
 const instanse = axios.create({
-  baseURL: "http://localhost:8000/"
+  // baseURL: "http://localhost:8000/"
+  baseURL: "https://epic-beaver-64c4c6.netlify.app:8000/",
 });
 
-instanse.interceptors.request.use(req => {
+instanse.interceptors.request.use((req) => {
   const user = store.getState().users.user;
-  if(user) req.headers["Authenticate"] = user.token;
+  if (user) req.headers["Authenticate"] = user.token;
 
   return req;
-})
+});
 
 export default instanse;
