fee20aa309126466f997f9739d7ac07a6784de49
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2022-03-08 20:23:28 +0600

    удалены не используемые импорты

--- src/components/CalendarForm/CalendarForm.js
+++ src/components/CalendarForm/CalendarForm.js
@@ -1,56 +1,68 @@
-import React, {useState, useEffect} from 'react';
-import 'date-fns';
-import { Button, Grid, makeStyles, TextField } from '@material-ui/core';
-import DateFnsUtils from '@date-io/date-fns';
+import React, { useState, useEffect } from "react";
+import "date-fns";
+import { Button, Grid, makeStyles, TextField } from "@material-ui/core";
+import DateFnsUtils from "@date-io/date-fns";
 import {
   MuiPickersUtilsProvider,
-  KeyboardTimePicker,
   KeyboardDatePicker,
-} from '@material-ui/pickers';
+} from "@material-ui/pickers";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
 }));
 
-const CalendarForm = ({onSubmit, calendar, btnText}) => {
+const CalendarForm = ({ onSubmit, calendar, btnText }) => {
   const classes = useStyles();
   const [state, setState] = useState({
-    title: '',
-    text: ''
+    title: "",
+    text: "",
   });
 
   useEffect(() => {
-    setState(prev => {
-      return {...prev, title: calendar.title, text: calendar.text};
+    setState((prev) => {
+      return { ...prev, title: calendar.title, text: calendar.text };
     });
     setSelectedDate(calendar.date);
   }, [calendar]);
 
   const inputChange = (e) => {
-    const {name, value} = e.target;
-    setState(prev => {
-      return {...prev, [name]: value}
-    })
-  }
+    const { name, value } = e.target;
+    setState((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
   const [selectedDate, setSelectedDate] = useState(calendar.date);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
-  
-  return(    
-    <Grid container direction='column'>
+
+  return (
+    <Grid container direction="column">
       <form className={classes.root} noValidate autoComplete="off">
         <Grid item>
-          <TextField label="Title" name='title' value={state.title} onChange={inputChange}/>
+          <TextField
+            label="Title"
+            name="title"
+            value={state.title}
+            onChange={inputChange}
+          />
         </Grid>
         <Grid item>
-          <TextField label="Text" multiline rows={4} variant="outlined" name='text' value={state.text} onChange={inputChange}/>
+          <TextField
+            label="Text"
+            multiline
+            rows={4}
+            variant="outlined"
+            name="text"
+            value={state.text}
+            onChange={inputChange}
+          />
         </Grid>
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <Grid item container justify="space-around">
@@ -64,23 +76,28 @@ const CalendarForm = ({onSubmit, calendar, btnText}) => {
               value={selectedDate}
               onChange={handleDateChange}
               KeyboardButtonProps={{
-                'aria-label': 'change date',
+                "aria-label": "change date",
               }}
-            />            
+            />
           </Grid>
         </MuiPickersUtilsProvider>
-        <Grid item >
-          <Button variant="contained" color="primary"
-                  onClick = {() => {
-                    const curCalendar = {
-                      _id: calendar._id,
-                      title: state.title,
-                      text: state.text,
-                      date: selectedDate
-                    };
-                    onSubmit(curCalendar);
-                    }}>{btnText}</Button>
-        </Grid>        
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              const curCalendar = {
+                _id: calendar._id,
+                title: state.title,
+                text: state.text,
+                date: selectedDate,
+              };
+              onSubmit(curCalendar);
+            }}
+          >
+            {btnText}
+          </Button>
+        </Grid>
       </form>
     </Grid>
   );
--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -1,36 +1,42 @@
-import { push } from 'connected-react-router';
-import axios from '../../axios-calendar';
-import { ADD_USER_ERROR, ADD_USER_SUCCESS, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR, LOGOUT_USER_SUCCESS, LOGOUT_USER, ADD_USER, LOGIN_USER } from "../actionTypes";
-
+import {
+  ADD_USER_ERROR,
+  ADD_USER_SUCCESS,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  LOGOUT_USER_SUCCESS,
+  LOGOUT_USER,
+  ADD_USER,
+  LOGIN_USER,
+} from "../actionTypes";
 
 export const addUserSuccess = (user) => {
-  return {type: ADD_USER_SUCCESS, user};
+  return { type: ADD_USER_SUCCESS, user };
 };
 
-export const addUserError = error => {
-  return {type: ADD_USER_ERROR, error};
+export const addUserError = (error) => {
+  return { type: ADD_USER_ERROR, error };
 };
 
-export const addUser = (user) => {  
-  return {type: ADD_USER, user};
-}
-
-export const loginUserSuccess = user => {  
-  return {type: LOGIN_USER_SUCCESS, user};
+export const addUser = (user) => {
+  return { type: ADD_USER, user };
 };
 
-export const loginUserFailure = error => {
-  return {type: LOGIN_USER_ERROR, error};
+export const loginUserSuccess = (user) => {
+  return { type: LOGIN_USER_SUCCESS, user };
 };
 
-export const loginUser = user => {
-  return {type: LOGIN_USER, user};
-}
+export const loginUserFailure = (error) => {
+  return { type: LOGIN_USER_ERROR, error };
+};
+
+export const loginUser = (user) => {
+  return { type: LOGIN_USER, user };
+};
 
 export const logoutUser = () => {
-  return {type: LOGOUT_USER};
-}
+  return { type: LOGOUT_USER };
+};
 
 export const logoutUserSuccess = () => {
-  return {type: LOGOUT_USER_SUCCESS};
-}
+  return { type: LOGOUT_USER_SUCCESS };
+};
